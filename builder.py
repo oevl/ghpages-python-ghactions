496c08a67e9c75d014b3009882621b67034b2c12
oevl <omar.vasquezlima@gmail.com>
2020-01-10 21:01:06 -0500

    Working YAML data file, HTML template and Python builder script

--- builder.py
+++ builder.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
 
 import yaml
+import jinja2
 
+# Import Meetup Data from YAML file
 with open('meetup.yaml') as f:
     
     data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
+    #print(data)
+
+# Load the template
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "template.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+renderedTemplate= template.render(
+	Meetup = data['Meetup'],
+	Saludo = data['Saludo'],
+	Actividades = data['Actividades'],
+	Datos = data['Datos'],
+	Expositores = data['Expositores'],
+	Patrocinantes = data['Patrocinantes'],
+	Testimonios = data ['Testimonios'],
+	MeetupURL = data['MeetupURL']
+)  # this is where to put args to the template renderer
+
+with open('index.html', 'w') as writer:
+	writer.write(renderedTemplate)
+
+
+
