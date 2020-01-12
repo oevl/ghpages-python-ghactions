abef2869cc2b89c1d945ccd7b935f3b7f507b659
oevl <omar.vasquezlima@gmail.com>
2020-01-11 19:10:19 -0500

    Reverting file deletion by action, from downloaded previous commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ghpages-python-ci
+Build a GitHub Pages site with Python, an HTML template and CI. Inspired by  danielo515/TW5-auto-publish2gh-pages 
---
+++ builder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import yaml
+import jinja2
+
+# Import Meetup Data from YAML file
+with open('meetup.yml') as f:
+    
+    data = yaml.load(f, Loader=yaml.FullLoader)
+	
+# Load the template
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "template.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+# Render the template
+renderedTemplate= template.render(
+	Meetup = data['Meetup'],
+	Saludo = data['Saludo'],
+	Actividades = data['Actividades'],
+	Datos = data['Datos'],
+	Expositores = data['Expositores'],
+	Patrocinantes = data['Patrocinantes'],
+	Testimonios = data ['Testimonios'],
+	MeetupURL = data['MeetupURL']
+)
+# Output the file
+with open('site/index.html', 'w') as writer:
+	writer.write(renderedTemplate)
+
+
+
